7a0a6b03ea989a5fefd73e8e5133f5cc8678bf79
Michael Gavrilenko <gavr.inbox@gmail.com>
2022-07-13 20:19:22 +0200

    test: is present button 'Add to Card'

--- test_items.py
+++ test_items.py
@@ -1,7 +1,10 @@
 import pytest
 from selenium.webdriver.common.by import By
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
 def test_presence_button_add_card(browser):
+    browser.get(link)
     try:
         browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
         present_btn = True
