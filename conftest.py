5553705801cc0311c78bad1f95c1625a74bb955a
Michael Gavrilenko <gavr.inbox@gmail.com>
2022-07-13 20:15:47 +0200

    fixtura start/exit browser, choose language and browser

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome', help="Choose browser: chrome or firefox")
@@ -10,16 +11,17 @@ def pytest_addoption(parser):
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     language_name = request.config.getoption("language")
-    link = f"http://selenium1py.pythonanywhere.com/{language_name}/catalogue/coders-at-work_207/"
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-        browser.get(link)
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language_name})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-        browser.get(link)
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language_name)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
